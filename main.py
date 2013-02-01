9bdfcbccde749176791c5f95670b8c5c1034ab46
Arie Bro <ariebro@gmail.com>
2013-02-01 15:18:39 +0200

    Added command line option parsing to enable running the app on a different port

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tornado.web, tornado.ioloop
 from sockjs.tornado import SockJSRouter, SockJSConnection
+from tornado.options import define, options
 
 from mongoengine import *
 
@@ -12,6 +13,8 @@ from studenttracker.rest.students import *
 from studenttracker.rest.tweets import *
 from studenttracker.rest.user import *
 
+define("port", default=APP_PORT, help="The port to listen on")
+
 # connect to mongodb (for mongoengine)
 connect(DB_NAME)
 
@@ -19,66 +22,67 @@ connect(DB_NAME)
 clients = set()
 
 class EchoConnection(SockJSConnection):
-	def on_open(self, info):
-		#self.session = get_session(info.cookies["session"].value)
-		#print "client connected: %s" % self.session["user_id"]
-		clients.add(self)
+    def on_open(self, info):
+        #self.session = get_session(info.cookies["session"].value)
+        #print "client connected: %s" % self.session["user_id"]
+        clients.add(self)
 
-	def on_message(self, msg):
-		print "received message %s" % msg
-		self.send(msg)
+    def on_message(self, msg):
+        print "received message %s" % msg
+        self.send(msg)
 
-	def on_close(self):
-		clients.remove(self)
+    def on_close(self):
+        clients.remove(self)
 
 
 if __name__ == "__main__":
-	EchoRouter = SockJSRouter(EchoConnection, '/echo')
+    EchoRouter = SockJSRouter(EchoConnection, '/echo')
 
-	application = tornado.web.Application([
-		# static directories
-		(r'/css/(.*)', tornado.web.StaticFileHandler, {'path': "css"}),
-		(r'/js/(.*)', tornado.web.StaticFileHandler, {'path': "js"}),
-		(r'/img/(.*)', tornado.web.StaticFileHandler, {'path': "img"}),
+    application = tornado.web.Application([
+        # static directories
+        (r'/css/(.*)', tornado.web.StaticFileHandler, {'path': "css"}),
+        (r'/js/(.*)', tornado.web.StaticFileHandler, {'path': "js"}),
+        (r'/img/(.*)', tornado.web.StaticFileHandler, {'path': "img"}),
 
-		# main page and other client pages
-		(r'/', MainHandler),
-		(r'/search', MainHandler),
-		(r'/search/.*', MainHandler),
-		(r'/tags/.+', MainHandler),
-		(r'/student/\w+', MainHandler),
-		(r'/lesson/\w+', MainHandler),
-		(r'/subject/\w+', MainHandler),
+        # main page and other client pages
+        (r'/', MainHandler),
+        (r'/search', MainHandler),
+        (r'/search/.*', MainHandler),
+        (r'/tags/.+', MainHandler),
+        (r'/student/\w+', MainHandler),
+        (r'/lesson/\w+', MainHandler),
+        (r'/subject/\w+', MainHandler),
 
-		# user pages
-		(r'/login', LoginHandler),
-		(r'/logout', LogoutHandler),
-		(r'/register', RegisterHandler),
+        # user pages
+        (r'/login', LoginHandler),
+        (r'/logout', LogoutHandler),
+        (r'/register', RegisterHandler),
 
-		# REST endpoints
-		# ##############
+        # REST endpoints
+        # ##############
 
-		# user model
-		(r'/user', UserHandler),
+        # user model
+        (r'/user', UserHandler),
 
-		# tweets collection and model
-		(r'/tweets', TweetsHandler),
-		(r'/tweets/(\w+)', TweetHandler),
+        # tweets collection and model
+        (r'/tweets', TweetsHandler),
+        (r'/tweets/(\w+)', TweetHandler),
 
-		# tweets collection and model
-		(r'/subjects', SubjectsHandler),
-		(r'/subjects/(\w+)', SubjectHandler),
+        # tweets collection and model
+        (r'/subjects', SubjectsHandler),
+        (r'/subjects/(\w+)', SubjectHandler),
 
-		# tweets collection and model
-		(r'/lessons', LessonsHandler),
-		(r'/lessons/(\w+)', LessonHandler),
+        # tweets collection and model
+        (r'/lessons', LessonsHandler),
+        (r'/lessons/(\w+)', LessonHandler),
 
-		# students collection and model
-		(r'/students', StudentsHandler),
-		(r'/students/(\w+)', StudentHandler),
-		#(r'/students/(\w+)/image', StudentImageHandler),
-	] + EchoRouter.urls, debug=True, static_path = "static", cookie_secret=COOKIE_SECRET)
+        # students collection and model
+        (r'/students', StudentsHandler),
+        (r'/students/(\w+)', StudentHandler),
+        #(r'/students/(\w+)/image', StudentImageHandler),
+        ] + EchoRouter.urls, debug=True, static_path = "static", cookie_secret=COOKIE_SECRET)
 
-	print 'Listening on port %s' % APP_PORT
-	application.listen(APP_PORT)
-	tornado.ioloop.IOLoop.instance().start()
+    options.parse_command_line()
+    print 'Listening on port %s' % options.port
+    application.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
